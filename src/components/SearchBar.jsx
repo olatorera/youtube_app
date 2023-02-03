31b21b6a0ec4e69fd98d3229d1dfbb3355ce357d
olatorera <66270040+olatorera@users.noreply.github.com>
2023-02-03 23:19:05 +0100

    navbar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Paper, IconButton } from "@mui/material";
-import { SearchIcon } from "@mui/icons-material";
+import SearchIcon from "@mui/icons-material/Search";
 import { border } from "@mui/system";
 
 const SearchBar = () => {
