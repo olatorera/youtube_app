ae3014e09796c456a24a1e7584e78a421e5b6b80
olatorera <66270040+olatorera@users.noreply.github.com>
2023-02-04 23:26:24 +0100

    feed  edit

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -5,7 +5,13 @@ import { Box, Stack, Typography } from "@mui/material";
 const Feed = () => {
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
-      <Box></Box>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: " 1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      ></Box>
     </Stack>
   );
 };
