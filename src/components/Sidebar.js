6bd228c7de2359d3a284bcb5aef9a5a2fb071b0a
olatorera <66270040+olatorera@users.noreply.github.com>
2023-02-19 02:11:17 +0100

    sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -20,10 +20,23 @@ const Sidebar = () => {
           className="category-btn"
           style={{
             background: category.name === selectedCategory && "FC1503",
+            color: "white",
           }}
+          key={category.name}
         >
-          <span>{category.icon}</span>
-          <span>{category.name}</span>
+          <span
+            style={{
+              color: category.name === selectedCategory ? "white" : "red",
+              marginRight: "15px",
+            }}
+          >
+            {category.icon}
+          </span>
+          <span
+            style={{ opacity: category.name === selectedCategory ? "1" : "0" }}
+          >
+            {category.name}
+          </span>
         </button>
       ))}
     </Stack>
